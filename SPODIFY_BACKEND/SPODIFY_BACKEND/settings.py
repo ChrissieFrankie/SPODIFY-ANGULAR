ed222f65394859bb27ccaeda7294e63a48adfddf
Christopher Francisco <cjfg00gl3@gmail.com>
2025-06-06 21:33:58 -0400

    UPDATE settings.py to load environment variables for database configuration

--- SPODIFY_BACKEND/SPODIFY_BACKEND/settings.py
+++ SPODIFY_BACKEND/SPODIFY_BACKEND/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,11 +79,11 @@ WSGI_APPLICATION = "SPODIFY_BACKEND.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "spodify",
-        "USER": "postgres",
-        "PASSWORD": os.environ.get("DB_PASSWORD"),
-        "HOST": "localhost",
-        "PORT": "5432",
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT"),
     }
 }
 
