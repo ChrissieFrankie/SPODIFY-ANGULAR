21497ef332f22abef6798f4fafea7029daf071b2
Christopher Francisco <cjfg00gl3@gmail.com>
2025-06-14 17:50:01 -0400

    update(loading.component.html, loading.component.scss, loading.component.ts): render a basic centered red sphere on a black background

--- SPODIFY_FRONTEND/src/app/features/loading/loading.component.ts
+++ SPODIFY_FRONTEND/src/app/features/loading/loading.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import * as THREE from 'three';
 
 @Component({
   selector: 'app-loading',
@@ -8,5 +9,22 @@ import { Component } from '@angular/core';
   styleUrl: './loading.component.scss'
 })
 export class LoadingComponent {
+  @ViewChild('canvas') canvas!: ElementRef<HTMLCanvasElement>; // canvas element reference
 
+  ngAfterViewInit() {
+    const canvas = this.canvas.nativeElement; // canvas native element reference
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    camera.position.z = 3;
+    const renderer = new THREE.WebGLRenderer({ canvas });
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+    const geometry = new THREE.SphereGeometry(1, 32, 32); // create sphere geometry
+    const material = new THREE.MeshBasicMaterial({ color: 0xFF0000 }); // create sphere material to be green
+    const sphere = new THREE.Mesh(geometry, material); // create sphere
+    scene.add(sphere); // add sphere to scene
+
+    renderer.render(scene, camera); // render scene with camera
+
+  }
 }
