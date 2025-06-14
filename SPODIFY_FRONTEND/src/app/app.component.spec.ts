252b9ed632f2a49d8fa495f985987a60f2f90399
Christopher Francisco <cjfg00gl3@gmail.com>
2025-06-14 16:56:08 -0400

    fix(app.component.spec.ts): correct test imports for standalone component

--- SPODIFY_FRONTEND/src/app/app.component.spec.ts
+++ SPODIFY_FRONTEND/src/app/app.component.spec.ts
@@ -1,10 +1,11 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { LoadingComponent } from './features/loading/loading.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [AppComponent, LoadingComponent],
     }).compileComponents();
   });
 
