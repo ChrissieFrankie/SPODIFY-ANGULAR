a4f249fd6f346fc67ea4a45a67164cca6368f09a
Christopher Francisco <cjfg00gl3@gmail.com>
2025-06-14 18:02:19 -0400

    update(loading.component.ts): adding lighting and shadow effects for the sphere

--- SPODIFY_FRONTEND/src/app/features/loading/loading.component.ts
+++ SPODIFY_FRONTEND/src/app/features/loading/loading.component.ts
@@ -20,9 +20,14 @@ export class LoadingComponent {
     renderer.setSize(window.innerWidth, window.innerHeight);
 
     const geometry = new THREE.SphereGeometry(1, 32, 32); // create sphere geometry
-    const material = new THREE.MeshBasicMaterial({ color: 0xFF0000 }); // create sphere material to be green
+    const material = new THREE.MeshStandardMaterial({ color: 0xFF0000 }); // create sphere material to be red responsive to light
     const sphere = new THREE.Mesh(geometry, material); // create sphere
     scene.add(sphere); // add sphere to scene
+
+    const light = new THREE.DirectionalLight(0xFFFFFF, 1); // create light
+    light.position.set(1, 1, -0.5); // set light position to be behind the sphere
+    light.castShadow = true; // cast shadow
+    scene.add(light); // add light to scene
 
     renderer.render(scene, camera); // render scene with camera
 
