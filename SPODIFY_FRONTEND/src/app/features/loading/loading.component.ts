1bcc46e3c8e73d9d51a7b31bf8eb7fa928008089
Christopher Francisco <cjfg00gl3@gmail.com>
2025-06-15 12:12:11 -0400

    update(loading.component.ts): to improve type annotations since its a typescript file

--- SPODIFY_FRONTEND/src/app/features/loading/loading.component.ts
+++ SPODIFY_FRONTEND/src/app/features/loading/loading.component.ts
@@ -1,6 +1,6 @@
 import { DecalGeometry } from 'three/examples/jsm/geometries/DecalGeometry.js';
 import { Component, ElementRef, ViewChild } from '@angular/core';
-import * as THREE from 'three';
+import {PCFSoftShadowMap, Texture, Scene, PerspectiveCamera, WebGLRenderer, SphereGeometry, MeshPhongMaterial, Mesh, TextureLoader,  Vector3, Euler, BoxGeometry, DirectionalLight, AmbientLight} from 'three';
 
 @Component({
   selector: 'app-loading',
@@ -13,46 +13,46 @@ export class LoadingComponent {
   @ViewChild('canvas') canvas!: ElementRef<HTMLCanvasElement>; // canvas element reference
 
   ngAfterViewInit() {
-    const canvas = this.canvas.nativeElement; // canvas native element reference
-    const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.5, 1000);
+    const canvas: HTMLCanvasElement = this.canvas.nativeElement; // canvas native element reference
+    const scene: Scene = new Scene();
+    const camera: PerspectiveCamera = new PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.5, 1000);
     camera.position.set(2, 2, 2); // set position directly above
     camera.lookAt(0, 0, 0); // look at the center of the sphere
-    const renderer = new THREE.WebGLRenderer({ canvas });
+    const renderer: WebGLRenderer = new WebGLRenderer({ canvas });
     renderer.setSize(window.innerWidth, window.innerHeight);
     renderer.shadowMap.enabled = true; // make it look more realistic
-    renderer.shadowMap.type = THREE.PCFSoftShadowMap;
-    const geometry = new THREE.SphereGeometry(1, 32, 32); // create sphere geometry
-    const material = new THREE.MeshPhongMaterial({ color: 0x1DB954, specular: 0x696969 }); // create sphere material to be green responsive to light
-    const sphere = new THREE.Mesh(geometry, material); // create sphere
+    renderer.shadowMap.type = PCFSoftShadowMap;
+    const geometry: SphereGeometry = new SphereGeometry(1, 32, 32); // create sphere geometry
+    const material: MeshPhongMaterial = new MeshPhongMaterial({ color: 0x1DB954, specular: 0x696969 }); // create sphere material to be green responsive to light
+    const sphere: Mesh = new Mesh(geometry, material); // create sphere
     sphere.castShadow = true;
     sphere.receiveShadow = true;
     scene.add(sphere); // add sphere to scene
-    const spotifySignalWaves = new THREE.TextureLoader().load('/SpotifySignalWaves.png'); // load the signal waves
-    const decalMaterial = new THREE.MeshPhongMaterial({map:spotifySignalWaves, transparent:true, specular: 0x696969 }); // material for transparent png
-    const decalPosition = new THREE.Vector3(0.8, 0.9, 1); // near the surface
-    const decalOrientation = new THREE.Euler(Math.PI / -4, Math.PI / 6, Math.PI / 6); // position facing the user
-    const decalSize = new THREE.Vector3(1.7, 1.7, 1.7); // self explanatory
-    const decalGeometry = new DecalGeometry(sphere, decalPosition, decalOrientation, decalSize); // create decal geometry
-    const decalMesh = new THREE.Mesh(decalGeometry, decalMaterial); // create the decal mesh
+    const spotifySignalWaves:Texture = new TextureLoader().load('/SpotifySignalWaves.png'); // load the signal waves
+    const decalMaterial:MeshPhongMaterial = new MeshPhongMaterial({ map: spotifySignalWaves, transparent: true, specular: 0x696969 }); // material for transparent png
+    const decalPosition:Vector3 = new Vector3(0.8, 0.9, 1); // near the surface
+    const decalOrientation:Euler = new Euler(Math.PI / -4, Math.PI / 6, Math.PI / 6); // position facing the user
+    const decalSize:Vector3 = new Vector3(1.7, 1.7, 1.7); // self explanatory
+    const decalGeometry:DecalGeometry = new DecalGeometry(sphere, decalPosition, decalOrientation, decalSize); // create decal geometry
+    const decalMesh:Mesh = new Mesh(decalGeometry, decalMaterial); // create the decal mesh
     scene.add(decalMesh);
-    const plateGeometry = new THREE.BoxGeometry(2, 0.1, 2); // create square plate geometry
-    const plateMaterial = new THREE.MeshPhongMaterial({ color: 0x696969 }); // fifty shades of grey lol
-    const plate = new THREE.Mesh(plateGeometry, plateMaterial);
+    const plateGeometry:BoxGeometry = new BoxGeometry(2, 0.1, 2); // create square plate geometry
+    const plateMaterial:MeshPhongMaterial = new MeshPhongMaterial({ color: 0x696969 }); // fifty shades of grey lol
+    const plate:Mesh = new Mesh(plateGeometry, plateMaterial);
     plate.position.set(0, -1, 0); // position just below the sphere
     plate.rotation.y = Math.PI / 2; // rotate the plate 90 degrees
     plate.receiveShadow = true;
     plate.castShadow = true;
     scene.add(plate);
-    const directionalLight = new THREE.DirectionalLight(0xFFFFFF, 1); // create light
+    const directionalLight:DirectionalLight = new DirectionalLight(0xFFFFFF, 1); // create light
     directionalLight.target = sphere; // aim the light at the sphere
     directionalLight.castShadow = true; // cast shadow
-    const ambientLight = new THREE.AmbientLight(0xFFFFFF, 0.1); // soft white ambient light
+    const ambientLight:AmbientLight = new AmbientLight(0xFFFFFF, 0.1); // soft white ambient light
     scene.add(ambientLight);
     scene.add(directionalLight); // add light to scene
-    let angle = 0; // light rotation
-    const radius = 2; // radius of circular path for light rotation
-    let yRotation = 0; // plate rotation
+    let angle:number = 0; // light rotation
+    const radius:number = 2; // radius of circular path for light rotation
+    let yRotation:number = 0; // plate rotation
     function animate() {
       requestAnimationFrame(animate); // request call for the next frame
       angle += 0.01; // move light along circular path
