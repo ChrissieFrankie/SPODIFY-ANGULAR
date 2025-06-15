6f99769d15315a068a17b847da2909a29053f36f
Christopher Francisco <cjfg00gl3@gmail.com>
2025-06-14 20:28:53 -0400

    update(loading.component.ts): rename light to directional light and add plate rotation animation

--- SPODIFY_FRONTEND/src/app/features/loading/loading.component.ts
+++ SPODIFY_FRONTEND/src/app/features/loading/loading.component.ts
@@ -17,20 +17,20 @@ export class LoadingComponent {
     const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.5, 1000);
     camera.position.set(2, 2, 2); // set position directly above
     camera.lookAt(0, 0, 0); // look at the center of the sphere
-    const renderer = new THREE.WebGLRenderer({ canvas});
+    const renderer = new THREE.WebGLRenderer({ canvas });
     renderer.setSize(window.innerWidth, window.innerHeight);
     renderer.shadowMap.enabled = true; // make it look more realistic
     renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 
     const geometry = new THREE.SphereGeometry(1, 32, 32); // create sphere geometry
-    const material = new THREE.MeshPhongMaterial({ color: 0x1DB954, specular:0x696969}); // create sphere material to be red responsive to light
+    const material = new THREE.MeshPhongMaterial({ color: 0x1DB954, specular: 0x696969 }); // create sphere material to be green responsive to light
     const sphere = new THREE.Mesh(geometry, material); // create sphere
     sphere.castShadow = true;
     sphere.receiveShadow = true;
     scene.add(sphere); // add sphere to scene
 
     const plateGeometry = new THREE.BoxGeometry(2, 0.1, 2); // create square plate geometry
-    const plateMaterial = new THREE.MeshPhongMaterial({ color: 0x696969}); // fifty shades of grey lol
+    const plateMaterial = new THREE.MeshPhongMaterial({ color: 0x696969 }); // fifty shades of grey lol
     const plate = new THREE.Mesh(plateGeometry, plateMaterial);
     plate.position.set(0, -1, 0); // position just below the sphere
     plate.rotation.y = Math.PI / 2; // rotate the plate 90 degrees
@@ -38,30 +38,35 @@ export class LoadingComponent {
     plate.castShadow = true;
     scene.add(plate);
 
-    const light = new THREE.DirectionalLight(0xFFFFFF, 1); // create light
-    light.target = sphere; // aim the light at the sphere
-    light.castShadow = true; // cast shadow
+    const directionalLight = new THREE.DirectionalLight(0xFFFFFF, 1); // create light
+    directionalLight.target = sphere; // aim the light at the sphere
+    directionalLight.castShadow = true; // cast shadow
 
     const ambientLight = new THREE.AmbientLight(0xFFFFFF, 0.1); // soft white ambient light
     scene.add(ambientLight);
 
-    scene.add(light); // add light to scene
+    scene.add(directionalLight); // add light to scene
 
-    let lightAngle = 0; // light rotation
+    let angle = 0; // light rotation
     const radius = 2; // radius of circular path for light rotation
 
+    let yRotation = 0; // plate rotation
 
     function animate() {
       requestAnimationFrame(animate); // request call for the next frame
-      lightAngle += 0.01; // move light along circular path
-      light.position.set(
-       0,// light is rotating around the x axis counter clockwise
-       radius * Math.cos(lightAngle), // light is oscillating the y axis
-        radius * Math.sin(lightAngle) // light is oscillating the z axis
+      angle += 0.01; // move light along circular path
+      directionalLight.position.set(
+        0,// light is rotating around the x axis counter clockwise
+        radius * Math.cos(angle), // light is oscillating the y axis
+        radius * Math.sin(angle) // light is oscillating the z axis
       );
+
+      yRotation += 0.01; // increase the y rotation
+      plate.rotation.y = angle; // rotate the plate 
       renderer.render(scene, camera); // render the scene from the perspective of the camera
     }
 
     animate(); // start the animation
+
   }
 }
