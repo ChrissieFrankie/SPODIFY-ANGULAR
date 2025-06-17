9caa1b371557bf39ce1cd059ee3bef613dc26ac2
Christopher Francisco <cjfg00gl3@gmail.com>
2025-06-16 23:55:26 -0400

    update(search-username.component): integrate search username feature into app component

--- SPODIFY_FRONTEND/src/app/app.component.ts
+++ SPODIFY_FRONTEND/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoadingComponent } from './features/loading/loading.component';
+import { SearchUsernameComponent } from './features/search-username/search-username.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LoadingComponent],
+  imports: [RouterOutlet, LoadingComponent, SearchUsernameComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
