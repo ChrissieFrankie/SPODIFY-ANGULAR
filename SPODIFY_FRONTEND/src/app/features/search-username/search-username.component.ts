6276b3c9b0e3303896cafdeaf7b12171ed781b75
Christopher Francisco <cjfg00gl3@gmail.com>
2025-06-16 23:31:00 -0400

    create(search-username.component): search username feature with HTML, SCSS, and unit tests

---
+++ SPODIFY_FRONTEND/src/app/features/search-username/search-username.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search-username',
+  imports: [],
+  templateUrl: './search-username.component.html',
+  styleUrl: './search-username.component.scss'
+})
+export class SearchUsernameComponent {
+
+}
