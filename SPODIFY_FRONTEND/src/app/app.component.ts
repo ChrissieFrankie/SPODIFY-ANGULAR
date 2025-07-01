b2b4ef328c1f423316e85583cf57a62073d98837
Christopher Francisco <cjfg00gl3@gmail.com>
2025-07-01 12:07:26 -0400

    update(app.component): remove unused HostListener import to clean up code

--- SPODIFY_FRONTEND/src/app/app.component.ts
+++ SPODIFY_FRONTEND/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostListener } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoadingComponent } from './features/loading/loading.component';
 import { SearchUsernameComponent } from './features/search-username/search-username.component';
