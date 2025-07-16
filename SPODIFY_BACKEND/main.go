6425bae95d64e346f5773c63c1a273b8b038910b
Christopher Francisco <cjfg00gl3@gmail.com>
2025-07-15 21:31:31 -0400

    update(main.go): add load dotenv and  load local Spotify client credentials functions

--- SPODIFY_BACKEND/main.go
+++ SPODIFY_BACKEND/main.go
@@ -3,8 +3,11 @@ package main
 import (
 	"encoding/json" // decoding json responses
 	"fmt"           // printing output
-	"os"            // operating system
-	"time"          // time management
+	"log"
+	"os"   // operating system
+	"time" // time management
+
+	"github.com/joho/godotenv" // loading .env
 )
 
 type TokenData struct { // holds access token/expiry
@@ -22,11 +25,28 @@ func loadLocalToken() (TokenData, error) { // saves the loal access token info i
 	return data, err
 }
 
+func loadDotEnv() {
+	err := godotenv.Load() // load .env file
+	if err != nil {
+		log.Fatalf("Error loading .env file: %v", err)
+	}
+}
+
+func loadLocalCredentials() (string, string) { // return spotify client id and client secret from .env
+	spotifyClientID := os.Getenv("SPOTIFY_CLIENT_ID")
+	spotifyClientSecret := os.Getenv("SPOTIFY_CLIENT_SECRET")
+	if spotifyClientID == "" || spotifyClientSecret == "" {
+		log.Fatal("SPOTIFY_CLIENT_ID or SPOTIFY_CLIENT_SECRET not found in .env file")
+	}
+	return spotifyClientID, spotifyClientSecret
+}
+
 func main() {
 	tokenData, err := loadLocalToken()                    // load local access token info into random access memory
 	if err != nil || time.Now().After(tokenData.Expiry) { // if an error occured or the token has expired
 		fmt.Println("Fetching new access token...")
-		// fetch the new token info
+		loadDotEnv()
+		loadLocalCredentials()
 		// save the new token info
 	}
 }
